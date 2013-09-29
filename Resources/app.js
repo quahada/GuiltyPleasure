54cfccd04d13a35fb0a63f55a4965115c83203a4
quahada <quahada@gmail.com>
2013-09-29 10:53:52 -0700

    removed 2nd tab & made app tab-less.

--- Resources/app.js
+++ Resources/app.js
@@ -23,38 +23,16 @@ var tab1 = Titanium.UI.createTab({
 
 var player = require('/player');
 player.init(win1);
+win1.open();
 
-//
-// create controls tab and root window
-//
-var win2 = Titanium.UI.createWindow({  
-    title:'Tab 2',
-    backgroundColor:'#fff'
-});
-var tab2 = Titanium.UI.createTab({  
-    icon:'KS_nav_ui.png',
-    title:'Tab 2',
-    window:win2
-});
-
-var label2 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 2',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
-
-win2.add(label2);
 
 
 
 //
 //  add tabs
 //
-tabGroup.addTab(tab1);  
-tabGroup.addTab(tab2);  
+//tabGroup.addTab(tab1);  
 
 
 // open tab group
-tabGroup.open();
+//tabGroup.open();
